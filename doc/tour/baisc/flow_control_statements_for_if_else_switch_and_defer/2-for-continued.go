3432d689b2b958639c3f4eddbf83de4c8cd52a37
lawxen <lawxen@outlook.com>
2021-10-13 22:34:55 +0800

    Update

---
+++ doc/tour/baisc/flow_control_statements_for_if_else_switch_and_defer/2-for-continued.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// The init and post statements are optional.
+func main() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
---
+++ doc/tour/baisc/flow_control_statements_for_if_else_switch_and_defer/4-forever.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/*
+
+Forever
+If you omit the loop condition it loops forever, so an infinite loop is compactly expressed.
+*/
+func main() {
+	for {
+		fmt.Println("hahaha")
+	}
+}
